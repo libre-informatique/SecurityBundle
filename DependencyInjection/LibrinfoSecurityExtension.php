045c081fe9187687bb3e0cc438c620ce2d58ed88
PapsOu <vivien.frasca@gmail.com>
2015-11-02 14:53:26 +0100

    started security configuration file in LibrinfoSecurityBundle. Default rules are overrideable in globa config.yml or in global security.yml under « jms_security_extra: method_access_control: » key

--- DependencyInjection/LibrinfoSecurityExtension.php
+++ DependencyInjection/LibrinfoSecurityExtension.php
@@ -2,10 +2,12 @@
 
 namespace Librinfo\SecurityBundle\DependencyInjection;
 
+use Librinfo\CoreBundle\DependencyInjection\DefaultParameters;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\Yaml\Yaml;
 
 /**
  * This is the class that loads and manages your bundle configuration
@@ -22,7 +24,16 @@ class LibrinfoSecurityExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.yml');
+        $loader->load('security.yml');
+
+        $currentSecurityParamters = $container->getParameter('security.access.method_access_control');
+        $bundleSecurityParameters = $container->getParameter('librinfo.security')['method_access_control'];
+
+        $container->setParameter(
+            'security.access.method_access_control',
+            array_merge($bundleSecurityParameters,$currentSecurityParamters)
+        );
     }
 }
